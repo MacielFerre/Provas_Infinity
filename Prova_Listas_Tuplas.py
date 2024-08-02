993b49b03655582af942c5ca3c14822850dda842
MacielFerre <87343930+MacielFerre@users.noreply.github.com>
2024-08-02 19:26:27 -0300

    Add files via upload

---
+++ Prova_Listas_Tuplas.py
@@ -0,0 +1,7 @@
+# [PYIA-A01] Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta, converta essa lista em uma tupla. 
+# Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+lista_frutas = ['Jambo', 'Umbu', 'Uva', 'Laranja', 'Acerola']
+tupla_frutas = tuple(lista_frutas)
+for i in tupla_frutas:
+    print(i)
